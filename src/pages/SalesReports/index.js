834f55fd5d1c24ca8830f3faaf016aad0f36a389
pedroHen14 <pedrohenrique_silvasantos@yahoo.com>
2021-05-02 08:54:54 -0300

    Criando paginas de relatorios

---
+++ src/pages/SalesReports/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Dashboard from "../../layouts/Dashboard";
+
+// import { Container } from './styles';
+
+function SalesReports() {
+  return <Dashboard title="Relatórios de vendas"></Dashboard>;
+}
+
+export default SalesReports;
