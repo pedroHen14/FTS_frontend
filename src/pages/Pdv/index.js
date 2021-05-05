2ff39acb8cffe4274b1eb21bf1a3b1f91d15aa89
pedroHen14 <pedrohenrique_silvasantos@yahoo.com>
2021-05-05 16:49:35 -0300

    Adicionando imagem ilustrativa, campo de inserção de código do produto e tabela com dados da venda

--- src/pages/Pdv/index.js
+++ src/pages/Pdv/index.js
@@ -1,16 +1,212 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Container, Content, Footer, Header, Image } from "./styles";
+import {
+  Container,
+  ContainerButton,
+  ContainerImage,
+  ContainerInput,
+  ContainerScreen,
+  Content,
+  Footer,
+  Header,
+  ImageLogo,
+  Screen,
+  ScreenHeader,
+} from "./styles";
 import imageLogo from "../../assets/FTS.png";
+import shoppingCart from "../../assets/shopping-cart.png";
+import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
 
 function Pdv() {
+  const [code, setCode] = useState("");
+
+  const handleInput = (e) => {
+    e.preventDefault();
+
+    setCode(e.target.value);
+
+    console.log(code);
+  };
+
   return (
     <Container>
       <Header>
-        <Image src={imageLogo} />
+        <ImageLogo src={imageLogo} />
+        <h1>Caixa aberto</h1>
       </Header>
-      <Content></Content>
-      <Footer></Footer>
+      <Content>
+        <ContainerInput>
+          <FormControl>
+            <InputLabel htmlFor="code">Código do produto</InputLabel>
+            <Input
+              id="code"
+              label="Código do produto"
+              type="text"
+              variant="outlined"
+              value={code}
+              onChange={handleInput}
+              required
+            />
+          </FormControl>
+          <div className="unit-value">
+            <h3>Valor unitário</h3>
+            <p>R$ 0,00</p>
+          </div>
+          <div className="total-value">
+            <h3>Total do item</h3>
+            <p>R$ 0,00</p>
+          </div>
+        </ContainerInput>
+        <ContainerImage>
+          <img src={shoppingCart} />
+        </ContainerImage>
+        <ContainerScreen>
+          <Screen>
+            <ScreenHeader>
+              <h2>Lista de produtos</h2>
+            </ScreenHeader>
+            <table>
+              <tr>
+                <td>
+                  <h4>N° item</h4>
+                </td>
+                <td>
+                  <h4>Código</h4>
+                </td>
+                <td>
+                  <h4>Descrição</h4>
+                </td>
+                <td>
+                  <h4>Qtd</h4>
+                </td>
+                <td>
+                  <h4>Vlr. unit.</h4>
+                </td>
+                <td>
+                  <h4>Total</h4>
+                </td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>372513</td>
+                <td>Coca cola 350ml</td>
+                <td>10</td>
+                <td>R$ 5,00</td>
+                <td>R$ 50,00</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>372513</td>
+                <td>Coca cola 350ml</td>
+                <td>10</td>
+                <td>R$ 5,00</td>
+                <td>R$ 50,00</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>372513</td>
+                <td>Coca cola 350ml</td>
+                <td>10</td>
+                <td>R$ 5,00</td>
+                <td>R$ 50,00</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>372513</td>
+                <td>Coca cola 350ml</td>
+                <td>10</td>
+                <td>R$ 5,00</td>
+                <td>R$ 50,00</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>372513</td>
+                <td>Coca cola 350ml</td>
+                <td>10</td>
+                <td>R$ 5,00</td>
+                <td>R$ 50,00</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>372513</td>
+                <td>Coca cola 350ml</td>
+                <td>10</td>
+                <td>R$ 5,00</td>
+                <td>R$ 50,00</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>372513</td>
+                <td>Coca cola 350ml</td>
+                <td>10</td>
+                <td>R$ 5,00</td>
+                <td>R$ 50,00</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>372513</td>
+                <td>Coca cola 350ml</td>
+                <td>10</td>
+                <td>R$ 5,00</td>
+                <td>R$ 50,00</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>372513</td>
+                <td>Coca cola 350ml</td>
+                <td>10</td>
+                <td>R$ 5,00</td>
+                <td>R$ 50,00</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>372513</td>
+                <td>Coca cola 350ml</td>
+                <td>10</td>
+                <td>R$ 5,00</td>
+                <td>R$ 50,00</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>372513</td>
+                <td>Coca cola 350ml</td>
+                <td>10</td>
+                <td>R$ 5,00</td>
+                <td>R$ 50,00</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>372513</td>
+                <td>Coca cola 350ml</td>
+                <td>10</td>
+                <td>R$ 5,00</td>
+                <td>R$ 50,00</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>372513</td>
+                <td>Coca cola 350ml</td>
+                <td>10</td>
+                <td>R$ 5,00</td>
+                <td>R$ 50,00</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>372513</td>
+                <td>Coca cola 350ml</td>
+                <td>10</td>
+                <td>R$ 5,00</td>
+                <td>R$ 50,00</td>
+              </tr>
+            </table>
+          </Screen>
+        </ContainerScreen>
+      </Content>
+      <ContainerButton>
+        <Button variant="contained" color="primary">
+          Cadastrar cliente
+        </Button>
+      </ContainerButton>
     </Container>
   );
 }
--- src/pages/Pdv/styles.js
+++ src/pages/Pdv/styles.js
@@ -11,33 +11,109 @@ export const Container = styled.div`
 
 export const Header = styled.header`
   display: flex;
-  height: 15vh;
+  height: 10vh;
   width: 100%;
   align-items: center;
-  justify-content: center;
+  color: white;
+  justify-content: space-around;
   position: relative;
   background-color: var(--primary);
 `;
 
-export const Image = styled.img`
-  width: 150px;
+export const ImageLogo = styled.img`
+  width: 100px;
   border-radius: 50%;
   position: absolute;
   top: 10px;
-  border: 5px solid var(--dark);
+  left: 50px;
+  border: 2px solid var(--dark);
 `;
 
 export const Content = styled.div`
   width: 100%;
   height: 85vh;
-  display: flex;
+  display: grid;
+
+  padding: 20px;
+  gap: 20px;
+  grid-template-areas:
+    "container-image container-input"
+    "container-screen container-screen";
 `;
 
-export const Footer = styled.footer`
-  width: 100%;
-  height: 10vh;
+export const ContainerInput = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
+
+  align-items: flex-start;
+  gap: 20px;
+  grid-area: "container-input";
+`;
+
+export const ContainerScreen = styled.div`
+  display: flex;
   align-items: center;
+  grid-area: "container-screen";
+  justify-items: center;
+`;
+
+export const ContainerImage = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  grid-area: "container-image";
+
+  img {
+    display: flex;
+    max-width: 350px;
+    min-width: 250px;
+    min-height: 250px;
+  }
+`;
+
+export const Screen = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  background-color: var(--light);
+  border-radius: 10px;
+  border: 1px solid var(--dark);
+
+  width: 100%;
+  gap: 20px;
+  min-height: 300px;
+
+  table {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    overflow: auto;
+    padding: 10px;
+    max-height: 400px;
+    min-width: 500px;
+  }
+
+  td {
+    width: 100px;
+    text-align: center;
+  }
+`;
+
+export const ScreenHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   background-color: var(--dark);
+  width: 100%;
+  color: white;
+  padding: 10px;
+`;
+
+export const ContainerButton = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
 `;
