32cf5786846d6beb973df4d22eea2b1106537202
pedroHen14 <pedrohenrique_silvasantos@yahoo.com>
2021-05-24 00:06:00 -0300

    Criando estrutura de pasta da pagina de estoque

---
+++ src/pages/Inventory/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Dashboard from "../../layouts/Dashboard";
+
+import { Container } from "./styles";
+
+function Inventory() {
+  return (
+    <Dashboard>
+      <Container></Container>
+    </Dashboard>
+  );
+}
+
+export default Inventory;
