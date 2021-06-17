9ce155540b101e5d80513626d94eb42121bdb18e
pedroHen14 <pedrohenrique_silvasantos@yahoo.com>
2021-06-16 23:08:00 -0300

    Finalizada a página pública que será fornecida ao cliente

--- src/components/CardProductClient/index.js
+++ src/components/CardProductClient/index.js
@@ -8,19 +8,55 @@ import {
   CardProduct,
   InfoContent,
 } from "./styles";
+
+import imageLogo from "../../assets/FTS.png";
+
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper.min.css";
 import "swiper/components/pagination/pagination.min.css";
-import SwiperCore, { Pagination } from "swiper/core";
+import SwiperCore, { Pagination, EffectCoverflow } from "swiper/core";
 
-SwiperCore.use([Pagination]);
+SwiperCore.use([EffectCoverflow, Pagination]);
 
 function CardProductClient() {
+  const arrayProducts = [
+    {
+      id: 1,
+      url: "https://images8.kabum.com.br/produtos/fotos/101288/microfone-gamer-hyperx-quadcast-antivibracao-led-preto-e-vermelho-hx-micqc-bk_1615553162_m.jpg",
+      name: "Microfone HyperX",
+    },
+    {
+      id: 2,
+      url: "https://images2.kabum.com.br/produtos/fotos/115582/smart-tv-led-50-4k-uhd-lg-4-hdmi-2-usb-wi-fi-bluetooth-thinq-ai-hdr-50un7310-_1596033073_m.jpg",
+      name: "TV Smart LG",
+    },
+    {
+      id: 3,
+      url: "https://images3.kabum.com.br/produtos/fotos/95803/95803_1522867513_index_m.jpg",
+      name: "HD Seagate 1TB",
+    },
+    {
+      id: 4,
+      url: "https://images5.kabum.com.br/produtos/fotos/148655/placa-de-video-zotac-gaming-geforce-rtx-3060-twin-edge-oc-15-gbps-12gb-gddr6-ray-tracing-zt-a30600h-10m_1613993479_m.jpg",
+      name: "Placa de Vídeo RTX 3060",
+    },
+    {
+      id: 5,
+      url: "https://images6.kabum.com.br/produtos/fotos/155486/smartphone-samsung-galaxy-m12-64gb-4gb-ram-octa-core-camera-quadrupla-5000mah-preto-sm-m127fzkszto_1622118086_m.jpg",
+      name: "Celular Galaxy M12",
+    },
+    {
+      id: 6,
+      url: "https://images1.kabum.com.br/produtos/fotos/128561/console-microsoft-xbox-series-s-500gb-branco-rrs-00006_1601067301_m.jpg",
+      name: "Xbox One S",
+    },
+  ];
+
   return (
     <Container>
       <InfoContainer>
         <InfoHeader>
-          <div></div>
+          <img src={imageLogo} alt="" />
         </InfoHeader>
         <InfoContent>
           <h1>Seu Produto</h1>
@@ -32,89 +68,33 @@ function CardProductClient() {
         </InfoContent>
       </InfoContainer>
       <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
         direction={"vertical"}
-        pagination={{
-          clickable: true,
-          hideOnClick: false,
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: false,
         }}
+        pagination={
+          (true,
+          {
+            clickable: true,
+          })
+        }
         className="mySwiper"
-        style={{
-          width: "400px",
-          height: "auto",
-        }}
       >
-        <SwiperSlide className="swiper-container">
-          <div className="card-image">
-            <img
-              src="https://www.extra-imagens.com.br/Control/ArquivoExibir.aspx?IdArquivo=1685908060"
-              alt=""
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiper-container">
-          <div className="card-image">
-            <img
-              src="https://www.extra-imagens.com.br/Control/ArquivoExibir.aspx?IdArquivo=1685908060"
-              alt=""
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiper-container">
-          <div className="card-image">
-            <img
-              src="https://www.extra-imagens.com.br/Control/ArquivoExibir.aspx?IdArquivo=1685908060"
-              alt=""
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiper-container">
-          <div className="card-image">
-            <img
-              src="https://www.extra-imagens.com.br/Control/ArquivoExibir.aspx?IdArquivo=1685908060"
-              alt=""
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiper-container">
-          <div className="card-image">
-            <img
-              src="https://www.extra-imagens.com.br/Control/ArquivoExibir.aspx?IdArquivo=1685908060"
-              alt=""
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiper-container">
-          <div className="card-image">
-            <img
-              src="https://www.extra-imagens.com.br/Control/ArquivoExibir.aspx?IdArquivo=1685908060"
-              alt=""
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiper-container">
-          <div className="card-image">
-            <img
-              src="https://www.extra-imagens.com.br/Control/ArquivoExibir.aspx?IdArquivo=1685908060"
-              alt=""
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiper-container">
-          <div className="card-image">
-            <img
-              src="https://www.extra-imagens.com.br/Control/ArquivoExibir.aspx?IdArquivo=1685908060"
-              alt=""
-            />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="swiper-container">
-          <div className="card-image">
-            <img
-              src="https://www.extra-imagens.com.br/Control/ArquivoExibir.aspx?IdArquivo=1685908060"
-              alt=""
-            />
-          </div>
-        </SwiperSlide>
+        {arrayProducts.map((p, index) => (
+          <SwiperSlide key={index}>
+            <div className="card-image">
+              <img src={p.url} alt={p.name} title={p.name} />
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </Container>
   );
